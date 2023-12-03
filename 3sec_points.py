d4d06fd1fae8ea75f29eb0d73fdd8719dcf238c1
john <33205097+johngunerli@users.noreply.github.com>
2023-12-03 18:12:05 -0500

    update_push

--- 3sec_points.py
+++ 3sec_points.py
@@ -1,36 +1,64 @@
 import cv2
 import mediapipe as mp
 import time
+import math
+import numpy as np
+
+# Function to calculate focal length in pixels from the field of view and resolution width
+def calculate_focal_length_in_pixels(resolution_width, FOV_degrees):
+    FOV_radians = FOV_degrees * (math.pi / 180)
+    focal_length_pixels = resolution_width / (2 * math.tan(FOV_radians / 2))
+    return focal_length_pixels
 
 # Initialize MediaPipe Pose.
 mp_pose = mp.solutions.pose
 pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
-# Function to estimate distance using shoulder landmarks.
-def estimate_distance(image_width, landmark1, landmark2):
-    KNOWN_WIDTH = 35  # Approximate shoulder width of an adult in cm
-    FOCAL_LENGTH = 800  # Focal length of the camera in pixels, needs calibration
-
+# Function to estimate horizontal offset and distance from the camera using shoulder landmarks.
+def estimate_position_and_distance(image_width, image_height, landmark1, landmark2, focal_length, known_width):
     # Calculate the pixel distance between landmarks
-    pixel_distance = ((landmark1.x - landmark2.x) ** 2 + (landmark1.y - landmark2.y) ** 2) ** 0.5 * image_width
+    pixel_distance = math.hypot((landmark1.x - landmark2.x) * image_width, (landmark1.y - landmark2.y) * image_height)
 
-    # Calculate distance
-    distance = (KNOWN_WIDTH * FOCAL_LENGTH) / pixel_distance
-    return distance
+    # Calculate distance from the camera
+    distance = (known_width * focal_length) / pixel_distance
 
-# List to store distance measurements
-distance_measurements = []
+    # Calculate average position (central point between the shoulders in image coordinates)
+    avg_x = (landmark1.x + landmark2.x) / 2 * image_width
+    avg_y = (landmark1.y + landmark2.y) / 2 * image_height
 
-# Timing and counting variables
+    # Calculate horizontal offset from the center of the image in pixels
+    center_x = image_width / 2
+    pixel_offset = avg_x - center_x
+
+    # Assuming a constant real-world field of view, calculate offset in cm (approximation)
+    distance_per_pixel = math.tan(math.radians(FOV / 2)) * 2 * distance / image_width
+    cm_offset = pixel_offset * distance_per_pixel
+
+    return cm_offset, distance, (int(avg_x), int(avg_y))
+
+# Constants provided by the user
+FOV = 60  # Field of View in degrees
+RW = 1280  # Resolution Width in pixels
+
+# Constants
+KNOWN_WIDTH = 35  # Approximate shoulder width of an adult in cm
+FOCAL_LENGTH = calculate_focal_length_in_pixels(RW, FOV)  # Focal length in pixels, calculated dynamically
+
+# List to store X and Y coordinates
+xy_coordinates = []
+
+# Counter for instances recorded
+instances_recorded = 0
+max_instances = 8  # Maximum number of instances to record
+
+# Timing variables
 start_time = time.time()
 time_interval = 0.4  # Time interval in seconds
-measurement_count = 0  # Counter for the number of measurements
-max_measurements = 8  # Maximum number of measurements
 
 # Capture video
 cap = cv2.VideoCapture(0)
 
-while cap.isOpened() and measurement_count < max_measurements:
+while cap.isOpened() and instances_recorded < max_instances:
     success, image = cap.read()
     if not success:
         break
@@ -43,16 +71,19 @@ while cap.isOpened() and measurement_count < max_measurements:
         landmark2 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
 
         ih, iw, ic = image.shape
-        distance = estimate_distance(iw, landmark1, landmark2)
+        cm_offset, distance, center_point = estimate_position_and_distance(iw, ih, landmark1, landmark2, FOCAL_LENGTH, KNOWN_WIDTH)
 
         if time.time() - start_time >= time_interval:
-            distance_measurements.append(distance)
-            measurement_count += 1
+            x_coord = (cm_offset)
+            y_coord = (distance)
+            xy_coordinates.append((x_coord, y_coord))
+            instances_recorded += 1
             start_time = time.time()
 
-        # Draw landmarks, connections, and distance text
+        # Draw landmarks, connections, and the center point
         mp.solutions.drawing_utils.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-        cv2.putText(image, f"Distance: {distance:.2f} cm", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.circle(image, center_point, 5, (255, 0, 0), -1)
+        cv2.putText(image, f"Offset: {cm_offset:.2f}cm, Distance: {distance:.2f}cm", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
     cv2.imshow('MediaPipe Pose', cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
     if cv2.waitKey(5) & 0xFF == 27:
@@ -60,9 +91,10 @@ while cap.isOpened() and measurement_count < max_measurements:
 
 cap.release()
 
-# Save the recorded distances to a text file
-with open("distance_measurements.txt", "w") as file:
-    for distance in distance_measurements:
-        file.write(f"{distance}\n")
+# Convert the list of coordinates to a NumPy array
+np_xy_coordinates = np.array(xy_coordinates)
+print("X and Y coordinates:", np_xy_coordinates)
 
-print("Distance measurements saved to distance_measurements.txt")
+# Optionally, save the NumPy array to a file
+np.save('xy_coordinates.npy', np_xy_coordinates)
+print("NumPy array saved to xy_coordinates.npy")
