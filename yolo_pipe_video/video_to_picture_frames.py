e4dde821a6bde6a13b9dfd126855413eeccce648
john <33205097+johngunerli@users.noreply.github.com>
2023-12-07 22:22:33 -0500

    v7 and final code

--- yolo_pipe_video/video_to_picture_frames.py
+++ yolo_pipe_video/video_to_picture_frames.py
@@ -1,36 +1,89 @@
-# write a program to get a video and convert it to frames, saved in a folder
-
 import cv2
 import os
-# def video_to_frames(video_path, frames_folder):
-#     # Opens the Video file
-#     os.makedirs(frames_folder, exist_ok=True)
-#     cap = cv2.VideoCapture(video_path)
-#     i = 0
-#     while(cap.isOpened()):
-#         ret, frame = cap.read()
-#         if ret == False:
-#             break
-#         cv2.imwrite(f'{frames_folder}/frame_{i}.jpg', frame)
-#         i += 1
- 
-#     cap.release()
-#     cv2.destroyAllWindows()
-    
-# video_to_frames("vids/testVid.mov", "frames")  # Uncomment this line to run the function
-
-os.makedirs("detected_video", exist_ok=True)
-
-import os
 from video_yolo_pipe import process_image
+
+# # Define video path and output directory
+# video_path = "./vids/testVideo.mp4"
+# output_dir = "frames"
+
+# # Create output directory if it doesn't exist
+# if not os.path.exists(output_dir):
+#     os.makedirs(output_dir)
+
+# # Open video capture object
+# cap = cv2.VideoCapture(video_path)
+
+# # Check if video opened successfully
+# if not cap.isOpened():
+#     print("Error opening video")
+#     exit(1)
+
+# # Get frame rate
+# fps = cap.get(cv2.CAP_PROP_FPS)
+
+# # Define frame count
+# frame_count = 0
+
+# # Read frame by frame
+# while True:
+#     # Capture frame-by-frame
+#     ret, frame = cap.read()
+
+#     # Check if frame read successfully
+#     if not ret:
+#         print("Video finished")
+#         break
+
+#     # Save frame as image in output directory
+#     frame_filename = os.path.join(output_dir, f"frame_{frame_count:05d}.jpg")
+#     cv2.imwrite(frame_filename, frame)
+#     # Increase frame count
+#     frame_count += 1
+
+# # Release capture object
+# cap.release()
+
+# print(f"Total frames saved: {frame_count}")
+# print(f"Frame rate: {fps:.2f} FPS")
+
+# Process saved frames
+os.makedirs("detected_video", exist_ok=True)
 
 frames_folder = 'frames'
 
 # Loop through each file in the frames folder
-for file in os.listdir(frames_folder):
+for frame_number, file in enumerate(os.listdir(frames_folder)):
     file_path = os.path.join(frames_folder, file)
     if file_path.lower().endswith(('.png', '.jpg', '.jpeg')):
-        processed_image = process_image(file_path)
+        processed_image = process_image(file_path, frame_number)
         # Now you can do something with the processed image
         cv2.imwrite(f'detected_video/{file}', processed_image)
-    
+
+# Combine processed frames back into a video
+input_dir = "detected_video"
+output_video = "combined_video.mp4"
+
+# Get all images in the input directory
+images = [os.path.join(input_dir, f) for f in os.listdir(input_dir) if f.endswith(".jpg")]
+
+# Check if there are any images
+if len(images) == 0:
+    print("No images found in the input directory")
+    exit(1)
+
+# Read the first image to get the frame size
+frame = cv2.imread(images[0])
+height, width, layers = frame.shape
+
+# Define video writer object
+video_writer = cv2.VideoWriter(output_video, cv2.VideoWriter_fourcc(*"XVID"), 30, (width, height))
+
+# Write each image to the video
+for image in images:
+    frame = cv2.imread(image)
+    video_writer.write(frame)
+
+# Release video writer object
+video_writer.release()
+
+print(f"Combined video saved as: {output_video}")
--- yolo_pipe_video/video_yolo_pipe.py
+++ yolo_pipe_video/video_yolo_pipe.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 import os
 import mediapipe as mp
-
+import shutil
 
 # Mediapipe processing
 FOV = 70  # Field of View in degrees
@@ -16,33 +16,51 @@ def calculate_focal_length_in_pixels(resolution_width, FOV_degrees):
     focal_length_pixels = resolution_width / (2 * math.tan(FOV_radians / 2))
     return focal_length_pixels
 
+
 FOCAL_LENGTH = calculate_focal_length_in_pixels(RW, FOV)
 
+
 def estimate_position_and_distance(image_width, image_height, landmark1, landmark2, focal_length, known_width):
-    pixel_distance = math.hypot((landmark1.x - landmark2.x) * image_width, (landmark1.y - landmark2.y) * image_height)
+    pixel_distance = math.hypot(
+        (landmark1.x - landmark2.x) * image_width, (landmark1.y - landmark2.y) * image_height)
     distance = (known_width * focal_length) / pixel_distance
     avg_x = (landmark1.x + landmark2.x) / 2 * image_width
     avg_y = (landmark1.y + landmark2.y) / 2 * image_height
     center_x = image_width / 2
     pixel_offset = avg_x - center_x
-    distance_per_pixel = math.tan(math.radians(FOV / 2)) * 2 * distance / image_width
+    distance_per_pixel = math.tan(math.radians(
+        FOV / 2)) * 2 * distance / image_width
     cm_offset = pixel_offset * distance_per_pixel
     return cm_offset, distance, (int(avg_x), int(avg_y))
 
 
+def clear_directory(directory):
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
+    os.makedirs(directory, exist_ok=True)
 
-def process_image(input_image_name):
+
+def process_image(input_image_name, frame_number):
+    # Clear directories for each frame
+    processed_directory = "yolo_person_processed"
+    detections_directory = "yolo_person_detections"
+    clear_directory(processed_directory)
+    clear_directory(detections_directory)
+
     # Load YOLO
-    net = cv2.dnn.readNet("../yolo_config/yolov3.weights", "../yolo_config/yolov3.cfg")
+    net = cv2.dnn.readNet("./yolov7_config/yolov7.weights",
+                          "./yolov7_config/yolov7.cfg")
     layer_names = net.getLayerNames()
-    output_layers = [layer_names[i - 1] for i in net.getUnconnectedOutLayers().flatten()]
+    output_layers = [layer_names[i - 1]
+                     for i in net.getUnconnectedOutLayers().flatten()]
 
     # Load image
     image = cv2.imread(input_image_name)
     height, width, channels = image.shape
 
     # Detecting objects
-    blob = cv2.dnn.blobFromImage(image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+    blob = cv2.dnn.blobFromImage(
+        image, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
     net.setInput(blob)
     outs = net.forward(output_layers)
 
@@ -71,12 +89,6 @@ def process_image(input_image_name):
 
     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
 
-    # Create directories
-    processed_directory = "yolo_person_processed"
-    detections_directory = "yolo_person_detections"
-    os.makedirs(processed_directory, exist_ok=True)
-    os.makedirs(detections_directory, exist_ok=True)
-
     # YOLO detection and cropping code...
     for i in range(len(boxes)):
         if i in indexes:
@@ -84,11 +96,11 @@ def process_image(input_image_name):
             crop_img = image[y:y+h, x:x+w]
             cv2.imwrite(f'{detections_directory}/person_{i}.jpg', crop_img)
 
-    
     xy_coordinates = []
 
     mp_pose = mp.solutions.pose
-    pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5)
+    pose = mp_pose.Pose(min_detection_confidence=0.5,
+                        min_tracking_confidence=0.5)
 
     for filename in os.listdir(detections_directory):
         if filename.endswith(".jpg") or filename.endswith(".png"):
@@ -102,36 +114,53 @@ def process_image(input_image_name):
                 landmark1 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.LEFT_SHOULDER]
                 landmark2 = results.pose_landmarks.landmark[mp_pose.PoseLandmark.RIGHT_SHOULDER]
 
-                
                 ih, iw, ic = image.shape
-                cm_offset, distance, center_point = estimate_position_and_distance(iw, ih, landmark1, landmark2, FOCAL_LENGTH, KNOWN_WIDTH)
+                cm_offset, distance, center_point = estimate_position_and_distance(
+                    iw, ih, landmark1, landmark2, FOCAL_LENGTH, KNOWN_WIDTH)
                 xy_coordinates.append((cm_offset, distance))
 
-                mp.solutions.drawing_utils.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+                mp.solutions.drawing_utils.draw_landmarks(
+                    image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
                 cv2.circle(image, center_point, 5, (255, 0, 0), -1)
-                cv2.putText(image, f"Offset: {cm_offset:.2f}cm, Distance: {distance:.2f}cm", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-                
-                print(f"Offset: {cm_offset:.2f}cm, Distance: {distance:.2f}cm")
-                # save these the offset and distance under filename.npy 
+                scale_factor = iw / 200  # Adjust the denominator to control scaling
+                font_scale = 0.5 * scale_factor
+                thickness = max(1, int(round(1 * scale_factor)))
+
+                # cv2.putText(image, f"x: {cm_offset:.2f}cm, y: {distance:.2f}cm", (
+                #     10, 30), cv2.FONT_HERSHEY_SIMPLEX, font_scale, (0, 255, 0), thickness, cv2.LINE_AA)
+
+                print(f"{cm_offset:.2f}cm,{distance:.2f}cm, frame {frame_number}")
+                # Save the offset and distance under filename.npy
                 os.makedirs("xy_data", exist_ok=True)
-                np.save(f"xy_data/{filename}.npy", np.array([cm_offset, distance]))
+                if os.path.exists(f"xy_data/frame_{frame_number}_1.npy"):
+                    np.save(
+                        f"xy_data/frame_{frame_number}_2.npy", np.array([cm_offset, distance]))
+                else: 
+                    np.save(
+                        f"xy_data/frame_{frame_number}_1.npy", np.array([cm_offset, distance]))
                 
 
-                processed_image_path = os.path.join(processed_directory, filename)
-                cv2.imwrite(processed_image_path, cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-                # Load the original image again
-                image = cv2.imread(input_image_name)
-                # Paste processed person images back onto the original image
-                for i in range(len(boxes)):
-                    if i in indexes:                        
-                        # if the file does not exist, skip it.
-                        try: 
-                            x, y, w, h = boxes[i]
-                            processed_person_image_path = os.path.join(processed_directory, f"person_{i}.jpg")
-                            processed_person_image = cv2.imread(processed_person_image_path)
-                            processed_person_image = cv2.resize(processed_person_image, (w, h))
-                            image[y:y+h, x:x+w] = processed_person_image 
-                        except cv2.error:
-                            pass
-                return image
-                
+                processed_image_path = os.path.join(
+                    processed_directory, filename)
+                cv2.imwrite(processed_image_path, cv2.cvtColor(
+                    image, cv2.COLOR_RGB2BGR))
+
+    # Load the original image again
+    image = cv2.imread(input_image_name)
+
+    # Paste processed person images back onto the original image
+    for i in range(len(boxes)):
+        if i in indexes:
+            try:
+                x, y, w, h = boxes[i]
+                processed_person_image_path = os.path.join(
+                    processed_directory, f"person_{i}.jpg")
+                processed_person_image = cv2.imread(
+                    processed_person_image_path)
+                processed_person_image = cv2.resize(
+                    processed_person_image, (w, h))
+                image[y:y+h, x:x+w] = processed_person_image
+            except:
+                pass
+
+    return image
